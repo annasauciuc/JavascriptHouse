5d1f15c12aa61db16155058e66c0ad52c8b2b6b0
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-10-03 21:38:31 +0100

    test

---
+++ Udemy exercises/Functions/reduce().js
@@ -0,0 +1,6 @@
+// const arr = [2, 3, 4, 5];
+// var doubledNumbers = arr.map(n => n * 2);
+// console.log(doubledNumbers);
+const numbers = [2, 3, 4, 5];
+const dollars = numbers.map(number => "$" + number);
+console.log(dollars);
